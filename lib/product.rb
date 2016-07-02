32f5195b701a97b2b4a70c41f55c3f375ed9e91c
Evaldas Grudzinskas <prohdo@icloud.com>
2016-07-02 21:56:02 +0300

    Add raise DuplicateProductError when duplicate found

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    raise DuplicateProductError, "#{title} already exists." if find_duplicate
+    @@products << self
+  end
+
+  def find_duplicate
+    @@products.map(&:title).include?(title)
   end
 end
