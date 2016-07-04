3605a628939a8273cb72a4efcd849a2a72a48121
Evaldas Grudzinskas <prohdo@icloud.com>
2016-07-04 22:08:10 +0300

    Add find_by Transaction class method

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -18,5 +18,14 @@ class Transaction
     def find(id)
       @@transactions.find { |transaction| transaction.id == id }
     end
+
+    # find_by takes one hash argument, where key is lookup method, which starts with
+    #   customer or product, and then after underscore must be existing attribute of customer or product
+    #   f.e.: find_by(cutomer_name: 'Walter Latimer') #=> reutrns an array with customers purchases
+    def find_by(lookup)
+      resource, attribute = lookup.keys[0].to_s.split('_')
+      lookup_value = lookup.values[0]
+      @@transactions.select { |transaction| transaction.send(resource).send(attribute) == lookup_value }
+    end
   end
 end
